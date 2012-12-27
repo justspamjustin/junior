1eb01e510c4d690a6fc77e02658179c6317d2092
Justin Martin <justspamjustin@gmail.com>
2012-12-27 12:39:09 -0700

    Pretty cool looking home page so far.

--- docs/javascripts/main.js
+++ docs/javascripts/main.js
@@ -1,37 +1,43 @@
 var HomeTemplate = [
-  '<header class="bar-title">',
-  ' <div class="header-animated">',
-  '   <h1 class="title">List Example</h1>',
-  ' </div>',
-  '</header>',
   '<div class="content">',
-  '  <ul class="list">',
-  '      <li>',
-  '          <a href="#" class="go-to-details">',
-  '              Go to details',
-  '          </a>',
-  '          <span class="chevron"></span>',
-  '      </li>',
-  '      <li>',
-  '          <a href="#" class="go-to-details">',
-  '              Go to details',
-  '          </a>',
-  '          <span class="chevron"></span>',
-  '      </li>',
-  '      <li>',
-  '          <a href="#" class="go-to-details">',
-  '              Go to details',
-  '          </a>',
-  '          <span class="chevron"></span>',
-  '      </li>',
-  '  </ul>',
+  ' <header class="junior-intro">',
+  '   <h1 class="junior-name"><i class="icon-umbrella"></i> Junior</h1>',
+  '   <p>A front-end framework for building html5 mobile apps with a native look and feel.</p>',
+  ' </header>',
+  ' <ul class="list divider-list"><li class="list-divider">Features</li></ul>',
+  ' <div class="carousel-container">',
+  '   <ul class="carousel-list">',
+  '     <li class="carousel-item native-look-and-feel">',
+  '       <summary>Transitions with a native look and feel.</summary>',
+  '       <div class="feature-icon"></div>',
+  '     </li>',
+  '     <li class="carousel-item carousel-content">',
+  '       <summary>Carousels using flickable.js</summary>',
+  '       <i class="icon-picture"></i>',
+  '     </li>',
+  '     <li class="carousel-item backbone-content">',
+  '       <summary>Integrated with Backbone.js</summary>',
+  '       <div class="feature-icon"></div>',
+  '     </li>',
+  '   </ul>',
+  ' </div>',
+  ' <div class="button-positive button-block show-more-button">Show me more!</div>',
   '</div>'
 ].join('\n');
 
 var HomeView = Jr.View.extend({
   render: function(){
     this.$el.html(HomeTemplate);
+    this.afterRender();
     return this;
+  },
+
+  afterRender: function() {
+    var after = function() {
+      this.$('.carousel-list').flickable({segments:3});
+    };
+    setTimeout(after,1);
+
   },
 
   events: {
